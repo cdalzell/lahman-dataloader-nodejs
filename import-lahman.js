e5e71a375367266aab5c9493aaf0b3f96a586c3c
Chris Dalzell <cdalzell@gmail.com>
2014-07-10 23:02:49 -0400

    Initial commit for the program file. Nothing but notes at the moment

---
+++ import-lahman.js
@@ -0,0 +1,15 @@
+/** process cmd line parameters */
+
+/** confirm db drop */
+
+/** unzip file if need be */
+
+/** create tables */
+
+/** if mongo, just going to loop over files and shove them on in */
+
+/** else special handlers for each table? there's got to be a better way! */
+
+/** Add indexes, probably going to want to keep this basic. maybe offer more "thorough" options later */
+
+/** delete extracted csv files */
